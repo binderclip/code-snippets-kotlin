88b44eb76546b97c8c764e565dfadc6bc1aaa2b5
Wythe Zhou <binderclip2014@gmail.com>
2017-08-09 15:05:39 +0800

    update list demo

--- src/ListSP/ListDemo.kt
+++ src/ListSP/ListDemo.kt
@@ -1,9 +1,23 @@
 package ListSP
 
-fun main(args: Array<String>) {
+fun listBasicOp() {
+    println("=== listBasicOp ===")
     var myList : MutableList<Int> = mutableListOf(1, 2, 3)
     myList.add(4)
     println(myList)
     println(myList.size)
     println(myList.count())     // 返回的就是 size
+}
+
+
+fun listAndType() {
+    println("=== listAndType ===")
+    val myList = mutableListOf<Int>()
+    print("$myList")
+}
+
+
+fun main(args: Array<String>) {
+    listBasicOp()
+    listAndType()
 }
