d7b1cfc4ac35e9a890b4c6ea7012aa261f354c47
Wythe Zhou <binderclip2014@gmail.com>
2017-08-16 17:47:05 +0800

    update string and map

--- src/main/kotlin/MapSP/MapDemo.kt
+++ src/main/kotlin/MapSP/MapDemo.kt
@@ -1,14 +1,48 @@
 package MapSP
-fun main(args: Array<String>) {
-    // create
+
+fun retrieveMap1(m: Map<String, String>) {
+    println("= retrieveMap1 =")
+    println("Keys:")
+    for (key in m.keys) {
+        println(key)
+    }
+    println("Values:")
+    m.values.map { println(it) }
+}
+
+fun retrieveMap2(m: Map<String, String>) {
+    println("= retrieveMap2 =")
+    m.entries.map { println(it.key + ": " + it.value) }
+}
+
+
+fun createMap(): Map<String, String> {
+    println("=== createMap ===")
     var m1 : MutableMap<String, String> = HashMap<String, String>()
     m1["Zara"] = "8"
     m1.put("Mahnaz", "31")
     m1.put("Ayan", "12")
     m1.put("Daisy", "14")
-    println()
     println("Map Elements")
     println("\t$m1")
+    return m1
+}
+
+
+fun createMap2() {
+    println("=== createMap2 ===")
+    val m1 = hashMapOf("a" to "aa", "b" to "bb")
+    println("m1: $m1")
+}
+
+
+fun mapWithDefault() {
+    println("=== mapWithDefault ===")
+}
+
+
+fun retrieveMap(m1: Map<String, String>) {
+    println("=== retrieveMap ===")
     println("Contain K Allen? " + m1.containsKey("Allen"))
     println("Contain K Zara? " + m1.containsKey("Zara"))
     println("Contain V 14? " + m1.containsValue("14"))
@@ -18,28 +52,46 @@ fun main(args: Array<String>) {
     println("Set: " + m1.entries)
     retrieveMap1(m1)
     retrieveMap2(m1)
+}
+
+
+fun mapToList(m1: Map<String, String>) {
+    println("=== mapToList ===")
     println("List: " + m1.toList())
-    // mapValues
+}
+
+
+fun mapValues(m1: Map<String, String>) {
+    println("=== mapValues ===")
     val m2 = m1.mapValues { "${it.value}@v" }
     println("After mapValues: $m2")
-    // list to map
+}
+
+
+fun listToMap() {
+    println("=== listToMap ===")
     val l1 = listOf('A' to 'a', 'B' to 'b')
-    println("List to map:" + l1.associate { it.first to it.second })
+    println("List to map: " + l1.associate { it.first to it.second })
 }
 
-fun retrieveMap1(m: Map<String, String>) {
-    println("== Retrieve Map 1 ==")
-    println("Keys:")
-    for (key in m.keys) {
-        println(key)
-    }
-    println("Values:")
-    m.values.map { println(it) }
-    println("====")
+
+fun mapFilter() {
+    println("=== mapFilter ===")
+    val m1 = mapOf("a" to 1, "b" to null, "c" to 3)
+    println("m1: $m1")
+    val m2 = m1.filter { it.value != null }
+    println("m2: $m2")
+    val m3 = m2.filter { it.value!! < 2}
+    println("m3: $m3")
 }
 
-fun retrieveMap2(m: Map<String, String>) {
-    println("== Retrieve Map 2 ==")
-    m.entries.map { println(it.key + ": " + it.value) }
-    println("====")
+
+fun main(args: Array<String>) {
+    val m1 = createMap()
+    retrieveMap(m1)
+    mapToList(m1)
+    mapValues(m1)
+    listToMap()
+    createMap2()
+    mapFilter()
 }
--- src/main/kotlin/StringSP/StringSplit.kt
+++ src/main/kotlin/StringSP/StringSplit.kt
@@ -1,6 +1,8 @@
 package StringSP
 
-fun main(args: Array<String>) {
+
+fun stringSplit() {
+    println("=== stringSplit ===")
     val s = "A;2017-08-16 11:06:42"
     val parts = s.split(";".toRegex())
     println(parts)
@@ -8,4 +10,19 @@ fun main(args: Array<String>) {
     val part2 = parts[1]
     println("part1: " + part1)
     println("part2: " + part2)
+}
+
+
+fun stringLength() {
+    println("=== stringLength ===")
+    val s = ""
+    val s2 = "x"
+    println("s.length: ${s.length}")
+    println("s2.length: ${s2.length}")
+}
+
+
+fun main(args: Array<String>) {
+    stringSplit()
+    stringLength()
 }
